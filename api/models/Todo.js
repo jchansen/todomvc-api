51e9970a5438fbeb9a1bb272ac17d91302fa02b5
Jason Hansen <jason_hansen@outlook.com>
2015-12-29 19:22:40 -0700

    expose todo endpoint

---
+++ api/models/Todo.js
@@ -0,0 +1,27 @@
+/**
+* Todo.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+var uuid = require('node-uuid');
+
+module.exports = {
+
+  attributes: {
+    id: {
+      type: 'string',
+      primaryKey: true,
+      defaultsTo: uuid.v4
+    },
+    title: {
+      type: 'string',
+      required: true
+    },
+    isCompleted: {
+      type: 'boolean',
+      defaultsTo: false
+    }
+  }
+};
+
